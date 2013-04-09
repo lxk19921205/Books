4d900776cda2fa372aaadb711a4497d191f5b916
Andriy Lin <lxk19921205@gmail.com>
2013-04-09 16:21:58 +0800

    prepare for the real start

--- web/main.py
+++ web/main.py
@@ -15,45 +15,12 @@
 # limitations under the License.
 #
 import webapp2
-import cgi
 
-form = """
-<h1> Enter some text to ROT13: </h1>
-<form method="post">
-    <textarea name="text">%(text)s</textarea>
-    <br />
-    <input type="submit">
-</form>
-"""
 
 class MainHandler(webapp2.RequestHandler):
-    def output(self, text=""):
-        self.response.write(form % {'text': text})
-
     def get(self):
-    	self.response.headers['Content-Type'] = "text/html"
-        self.output()
+        self.response.out.write("Hello World")
 
-    def rot13(self, s):
-        lower = "abcdefghijklmnopqrstuvwxyz"
-        upper = lower.upper()
-        result = ""
-        for c in s:
-            if c in lower:
-                index = lower.find(c)
-                result = result + lower[(index + 13) % 26]
-            elif c in upper:
-                index = upper.find(c)
-                result = result + upper[(index + 13) % 26]
-            else:
-                result = result + c
-                
-        return result
-
-    def post(self):
-        t = self.request.get('text')
-        temp = self.rot13(t)
-        self.output(cgi.escape(temp))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
