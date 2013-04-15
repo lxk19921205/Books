a6df8a79151fd1d21342f362cd4bf699b9e0cdf5
Andriy Lin <lxk19921205@gmail.com>
2013-04-15 10:33:12 +0800

    add errors.py which contains all defined-by-me errors thay may occur

---
+++ web/utils/errors.py
@@ -0,0 +1,7 @@
+'''
+@author: Andriy Lin
+@description: Provide all kinds of *errors* that may occur while running the application
+'''
+# TODO fulfill here after reading the exception chapters of python book
+class FetchDataError(object):
+    pass
