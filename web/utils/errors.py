8766cc6ae9034b3c4ef4ce987d3547e587e3e5bf
Andriy Lin <lxk19921205@gmail.com>
2013-04-15 22:37:10 +0800

    add error hierarchy

--- web/utils/errors.py
+++ web/utils/errors.py
@@ -1,7 +1,27 @@
 '''
 @author: Andriy Lin
-@description: Provide all kinds of *errors* that may occur while running the application
+@description: The hierarchy of all *errors* that may occur while running the application.
 '''
-# TODO fulfill here after reading the exception chapters of python book
-class FetchDataError(object):
+
+class ABError(Exception):
+    """ The basic exception for this project. Stands for *AndriyBooksException*. """
     pass
+
+
+class FetchDataError(ABError):
+    """ Error fetching data from a source online. """
+
+    def __init__(self, link, error_code=None):
+        """ @param link: the url to fetch data.
+            @param error_code: the error code provided by the resource url.
+        """
+        self.link = link
+        self.error_code = error_code
+
+
+class ParseJsonError(ABError):
+    """ Error parsing the provided json into a valid data structure. """
+
+    def __init__(self, res_id):
+        """ @param res_id: may be the book_id of douban. """
+        self.res_id = res_id
