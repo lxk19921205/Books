53cb892bbfc452cc4f18896fa20574b68c33f19f
Andriy Lin <lxk19921205@gmail.com>
2013-05-11 16:21:38 +0800

    the /me page now displays in the nav framework

--- web/pages/me.py
+++ web/pages/me.py
@@ -6,25 +6,24 @@
 import webapp2
 
 import auth
+import utils
 
 
 class MeHandler(webapp2.RequestHandler):
     """ Handling '/me' or '/me/' """
 
     def _output(self, name, value):
-        self.response.out.write(name + ": ")
-        self.response.out.write(value)
-        self.response.out.write('<br/>')
+        self.html += unicode(name + ": ")
+        self.html += unicode(value)
+        self.html += u'<br/>'
 
     def _output_link(self, name, link):
-        html = "<a href='%s'>%s</a>" % (link, name)
-        self.response.out.write(html)
-        self.response.out.write('<br/>')
+        self.html += u"<a href='%s'>%s</a>" % (link, name)
+        self.html += u'<br/>'
 
     def _output_image(self, link):
-        html = "<img src='%s'/>" % link
-        self.response.out.write(html)
-        self.response.out.write('<br/>')
+        self.html += "<img src='%s'/>" % link
+        self.html += '<br/>'
 
     def get(self):
         email = auth.get_email_from_cookies(self.request.cookies)
@@ -33,6 +32,8 @@ class MeHandler(webapp2.RequestHandler):
             return
 
         user = auth.user.User.get_by_email(email)
+        self.html = ""
+
         self._output('Email', user.email)
         self._output('Douban id', user.douban_id)
         self._output('Douban uid', user.douban_uid)
@@ -42,3 +43,15 @@ class MeHandler(webapp2.RequestHandler):
         self._output('Signature', user.douban_signature)
         self._output('Description', user.douban_description)
         self._output('Created at', user.douban_created_time)
+
+#         jinja_env = utils.get_jinja_env()
+        import os
+        import jinja2
+        jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.realpath("./static/html/")),
+                               autoescape=False)
+        template = jinja_env.get_template("me.html")
+        context = {
+            'user': user,
+            'html': self.html
+        }
+        self.response.out.write(template.render(context))
