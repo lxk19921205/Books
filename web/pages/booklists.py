8878d762363eac015a026923b9518118ba48a868
Andriy Lin <lxk19921205@gmail.com>
2013-05-12 14:32:34 +0800

    finish the framework of booklists

--- web/pages/booklists.py
+++ web/pages/booklists.py
@@ -5,21 +5,52 @@
 
 import webapp2
 
+import utils
+import auth
+
+
 class _BookListHandler(webapp2.RequestHandler):
     """ The base handler for all the Book list handler. """
 
     def get(self):
-        self.response.out.write("Booklist: " + self.id)
+        email = auth.get_email_from_cookies(self.request.cookies)
+        if email:
+            user = auth.user.User.get_by_email(email)
+            template = utils.get_jinja_env().get_template("booklist.html")
+            context = {
+                'user': user,
+                'title': self.title,
+                'active_nav': self.active_nav,
+                'books': self._prepare_books()
+            }
+            self.response.out.write(template.render(context))
+        else:
+            self.redirect('/login')
+
+    def _prepare_books(self):
+        """ For subclasses to override, specifying data source, return the books in this list. """
+        raise NotImplementedError()
 
 
 class ReadingListHandler(_BookListHandler):
-    id = "Reading"
-    pass
+    title = "Reading List"
+    active_nav = "Reading"
+
+    def _prepare_books(self):
+        return None
+
 
 class InterestedListHandler(_BookListHandler):
-    id = "Interested"
-    pass
+    title = "Interested List"
+    active_nav = "Interested"
+
+    def _prepare_books(self):
+        return None
+
 
 class DoneListHandler(_BookListHandler):
-    id = "Done"
-    pass
+    title = "Done List"
+    active_nav = "Done"
+
+    def _prepare_books(self):
+        return None
