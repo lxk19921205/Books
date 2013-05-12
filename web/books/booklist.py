019ea7a46e09cef68afc7b450d8da4c0f4a83097
Andriy Lin <lxk19921205@gmail.com>
2013-05-12 23:46:28 +0800

    add some properties and accessing methods in BookList

--- web/books/booklist.py
+++ web/books/booklist.py
@@ -18,5 +18,42 @@ class BookList(db.Model):
     '''
 
     name = db.StringProperty(required=True)
-    books = db.StringListProperty(required=True)
-    # TODO some more attributes?
+    user = db.ReferenceProperty(required=True)
+    isbns = db.StringListProperty(required=True)
+    note = db.TextProperty()
+    # TODO tags may not be useful currently
+    # tags = db.StringListProperty()
+
+    @db.transactional
+    def add_book(self, book):
+        if book.isbn in self.isbns:
+            return False
+        else:
+            self.isbns.append(book.isbn)
+            self.put()
+            return True
+
+    @db.transactional
+    def remove_book(self, book):
+        if book.isbn in self.isbns:
+            self.isbns.remove(book.isbn)
+            self.put()
+        return True
+
+    @db.transactional
+    def set_name(self, name):
+        if name:
+            self.name = name
+            self.put()
+            return True
+        else:
+            return False
+
+    @db.transactional
+    def set_note(self, note):
+        if note:
+            self.note = note
+            self.put()
+            return True
+        else:
+            return False
