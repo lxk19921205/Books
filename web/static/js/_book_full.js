2e88243e2635f7cde7bd3f881c7eb027c8858fdd
Andriy Lin <lxk19921205@gmail.com>
2013-05-22 22:37:19 +0800

    use "range" input to set rating

--- web/static/js/_book_full.js
+++ web/static/js/_book_full.js
@@ -27,4 +27,26 @@ ab.confirm_list_selection = function() {
 	}
 
 	return true;
-}
+};
+
+
+/**
+ * Init the relationship between the rating "range" and its "output".
+ */
+ab.initRatingForm = function() {
+	// the following lines are for rating selection
+	var rating_form = document.getElementById('rating_form');
+	var rating_range = document.getElementById('rating_range');
+	var rating_output = document.getElementById('rating_output');
+
+	rating_form.oninput = function() {
+		if (rating_range.value == 0) {
+			rating_output.value = "clear";
+		} else if (rating_range.value == 1) {
+			rating_output.value = rating_range.value + " star";
+		} else {
+			rating_output.value = rating_range.value + " stars";
+		}
+	};
+	rating_form.oninput();
+};
